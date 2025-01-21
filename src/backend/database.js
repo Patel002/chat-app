6d6f693ad95ab3aa51b45bc413fec5b14eab959b
Patel002 <patelvatsal79715@gmail.com>
2025-01-21 15:36:24 +0530

    cloud database config

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2"
 
 const dbConfig = {
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "dhtsol",
-    database: process.env.DB_NAME || "chat_app",
+    host: process.env.DB_HOST || "bppnidd9dftravakxivc-mysql.services.clever-cloud.com",
+    user: process.env.DB_USER || "untlslcyornlvdx1",
+    password: process.env.DB_PASSWORD || "IlqALEEfzMSOtxWPwgKW",
+    database: process.env.DB_NAME || "bppnidd9dftravakxivc",
     port: process.env.DB_PORT || 3306   
 }
 
