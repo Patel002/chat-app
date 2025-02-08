b1e54ae4bac5bf02cf31384d5bca9a119922aa11
Patel002 <patelvatsal79715@gmail.com>
2025-02-08 17:56:45 +0530

    optimized

--- src/frontend/js/videoCall.js
+++ src/frontend/js/videoCall.js
@@ -69,6 +69,7 @@ async function startCall() {
                     bitrateMax: 1500,
                 },
                facingMode: 'user',
+               optimizationMode: "detail"
             })
         ]);
 
