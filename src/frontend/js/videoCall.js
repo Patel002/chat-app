a7c33effbd4a359929f64eb192700fa6825f41a1
Patel002 <patelvatsal79715@gmail.com>
2025-02-08 18:04:26 +0530

    optimization of video call

--- src/frontend/js/videoCall.js
+++ src/frontend/js/videoCall.js
@@ -64,6 +64,7 @@ async function startCall() {
             AgoraRTC.createCameraVideoTrack({
                 encoderConfig: "720p_15",
                 facingMode: 'user',
+                optimizationMode: "detail"
             })
         ]);
 
